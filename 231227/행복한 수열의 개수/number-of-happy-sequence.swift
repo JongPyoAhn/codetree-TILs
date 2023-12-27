ed19f4030c759b54e41446cfca0b5b2f3576756f
jongpyo Ahn <68585628+JongPyoAhn@users.noreply.github.com>
2023-12-27 17:24:26 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231227/행복한 수열의 개수/number-of-happy-sequence.swift
@@ -0,0 +1,49 @@
+let nm = readLine()!.split(separator: " ").map { Int(String($0))! }
+let n = nm[0]
+let m = nm[1]
+var arr = [[Int]]()
+
+for i in 0..<n {
+    arr.append(readLine()!.split(separator: " ").map { Int(String($0))! })
+}
+
+func search() -> Int {
+    var totalSequenceCount = 0
+    
+    for i in 0..<n { 
+        var continuousCount = 1
+        var prevNumber = -1
+        ///세로 행복수열 확인
+        for j in 0..<n {
+            if prevNumber == arr[i][j] {
+                continuousCount += 1
+                if continuousCount == m {
+                    totalSequenceCount += 1
+                    break
+                }
+            } else {
+                continuousCount = 1
+            }
+            prevNumber = arr[i][j]
+        }
+
+        continuousCount = 1
+        prevNumber = -1
+        ///가로 행복수열 확인
+        for j in 0..<n {
+            if prevNumber == arr[j][i] {
+                continuousCount += 1
+                if continuousCount == m {
+                    totalSequenceCount += 1
+                    break
+                }
+            } else {
+                continuousCount = 1
+            }
+            prevNumber = arr[j][i]
+        }
+    }
+    return totalSequenceCount
+}
+
+print(search())
