b52181f0ca6ad1bc0b2b0c77ce8edbc301a1fa32
jongpyo Ahn <68585628+JongPyoAhn@users.noreply.github.com>
2023-12-30 01:39:26 +0900

    Title: 기울어진 직사각형, Time: 32ms, Memory: 0MB, Status: Passed - CodeTree

--- 231230/기울어진 직사각형/slanted-rectangle.swift
+++ 231230/기울어진 직사각형/slanted-rectangle.swift
@@ -4,6 +4,19 @@ for _ in 0..<n {
     arr.append(readLine()!.split(separator: " ").map { Int(String($0))! })
 }
 
+struct Rectangle {
+  let point: Point
+  let bottomLength: Int
+  let rightLength: Int
+  let topLength: Int
+  let leftLength: Int
+  
+  func compare() -> Bool {
+    guard bottomLength == topLength && leftLength == rightLength else { return false }
+    return true
+  }
+}
+
 struct Point {
     let x: Int
     let y: Int
@@ -12,79 +25,166 @@ struct Point {
 let dx = [-1, -1, 1, 1]
 let dy = [1, -1, -1, 1]
 
-func rightUp(x: Int, y: Int, bottom: Int, sum: Int) -> Int {
-  let nx = x + dx[0]
-  let ny = y + dy[0]
+func rightUp(rectangle: Rectangle, sum: Int) -> Int {
+  let nx = rectangle.point.x + dx[0]
+  let ny = rectangle.point.y + dy[0]
   
-  if nx < 0 || ny < 0 || nx >= n || ny >= n || bottom >= n {
+  if nx < 0 || ny < 0 || nx >= n || ny >= n || rectangle.bottomLength >= n {
     return 0
   }
 
   let nSum = sum + arr[nx][ny]
 
-  let answer1 = leftUp(x: nx, y: ny, bottom: bottom, right: 1, sum: nSum)
+  let nPoint = Point(x: nx, y: ny)
+  let answer1 = leftUp(
+    rectangle: Rectangle(
+      point: nPoint,
+      bottomLength: rectangle.bottomLength,
+      rightLength: 1,
+      topLength: 0,
+      leftLength: 0
+    ),
+    sum: nSum
+  )
   
-  let answer2 = rightUp(x: nx, y: ny, bottom: bottom + 1, sum: nSum)
+  let answer2 = rightUp(
+    rectangle: Rectangle(
+      point: nPoint,
+      bottomLength: rectangle.bottomLength + 1,
+      rightLength: rectangle.rightLength,
+      topLength: rectangle.topLength,
+      leftLength: rectangle.leftLength
+    ),
+    sum: nSum
+  )
   
   return max(answer1, answer2)
 }
 
-func leftUp(x: Int, y: Int, bottom: Int, right: Int, sum: Int) -> Int {
-  let nx = x + dx[1]
-  let ny = y + dy[1]
+func leftUp(rectangle: Rectangle, sum: Int) -> Int {
+  let nx = rectangle.point.x + dx[1]
+  let ny = rectangle.point.y + dy[1]
   
-  if nx < 0 || ny < 0 || nx >= n || ny >= n || right >= n {
+  if nx < 0 || ny < 0 || nx >= n || ny >= n || rectangle.rightLength >= n {
     return 0
   }
 
   let nSum = sum + arr[nx][ny]
 
-  let answer1 = leftDown(x: nx, y: ny, bottom: bottom, right: right, top: 1, sum: nSum)
+  let nPoint = Point(x: nx, y: ny)
   
-  let answer2 = leftUp(x: nx, y: ny, bottom: bottom, right: right + 1, sum: nSum)
+  let answer1 = leftDown(
+    rectangle:
+      Rectangle(
+        point: nPoint,
+        bottomLength: rectangle.bottomLength,
+        rightLength: rectangle.rightLength,
+        topLength: 1,
+        leftLength: 0
+      ),
+    sum: nSum
+  )
+  
+  let answer2 = leftUp(
+    rectangle: 
+      Rectangle(
+        point: nPoint,
+        bottomLength: rectangle.bottomLength,
+        rightLength: rectangle.rightLength + 1,
+        topLength: 0,
+        leftLength: 0
+      ),
+    sum: nSum
+  )
   
   return max(answer1, answer2)
 }
 
-func leftDown(x: Int, y: Int, bottom: Int, right: Int, top: Int, sum: Int) -> Int {
-  let nx = x + dx[2]
-  let ny = y + dy[2]
+func leftDown(rectangle: Rectangle, sum: Int) -> Int {
+  let nx = rectangle.point.x + dx[2]
+  let ny = rectangle.point.y + dy[2]
   
-  if nx < 0 || ny < 0 || nx >= n || ny >= n || top >= n {
+  if nx < 0 || ny < 0 || nx >= n || ny >= n || rectangle.topLength >= n {
     return 0
   }
 
   let nSum = sum + arr[nx][ny]
 
-  let answer1 = rightDown(x: nx, y: ny, bottom: bottom, right: right, top: top, left: 1, sum: nSum)
+  let nPoint = Point(x: nx, y: ny)
+
+  let answer1 = rightDown(
+    rectangle:
+      Rectangle(
+        point: nPoint,
+        bottomLength: rectangle.bottomLength,
+        rightLength: rectangle.rightLength,
+        topLength: rectangle.topLength,
+        leftLength: 1
+      ),
+    sum: nSum
+  )
   
-  let answer2 = leftDown(x: nx, y: ny, bottom: bottom, right: right, top: top + 1, sum: nSum)
+  let answer2 = leftDown(
+    rectangle:
+      Rectangle(
+        point: nPoint,
+        bottomLength: rectangle.bottomLength,
+        rightLength: rectangle.rightLength,
+        topLength: rectangle.topLength + 1,
+        leftLength: 0
+      ),
+    sum: nSum
+  )
 
   return max(answer1, answer2)
 }
 
-func rightDown(x: Int, y: Int, bottom: Int, right: Int, top: Int, left: Int, sum: Int) -> Int {
-  let nx = x + dx[3]
-  let ny = y + dy[3]
+func rightDown(rectangle: Rectangle, sum: Int) -> Int {
+  let nx = rectangle.point.x + dx[3]
+  let ny = rectangle.point.y + dy[3]
   
-  if nx < 0 || ny < 0 || nx >= n || ny >= n || left >= n {
+  if nx < 0 || ny < 0 || nx >= n || ny >= n || rectangle.leftLength >= n {
     return 0
   }
-
+  
   let nSum = sum + arr[nx][ny]
 
-  if bottom == top && left == right {
+  let nPoint = Point(x: nx, y: ny)
+
+  if rectangle.compare() {
     return nSum
   }
 
-  return rightDown(x: nx, y: ny, bottom: bottom, right: right, top: top, left: left + 1, sum: nSum)
+  return rightDown(
+    rectangle:
+      Rectangle(
+        point: nPoint,
+        bottomLength: rectangle.bottomLength,
+        rightLength: rectangle.rightLength,
+        topLength: rectangle.topLength,
+        leftLength: rectangle.leftLength + 1
+      ),
+    sum: nSum
+  )
 }
 
 var result = 0
 
 for i in 0..<n {
   for j in 0..<n {
-    result = max(result, rightUp(x: i, y: j, bottom: 1, sum: 0))
+    result = max(
+      result,
+      rightUp(
+        rectangle: Rectangle(
+          point: Point(x: i, y: j),
+          bottomLength: 1,
+          rightLength: 0,
+          topLength: 0,
+          leftLength: 0
+        ),
+        sum: 0
+      )
+    )
   }
 }
 
